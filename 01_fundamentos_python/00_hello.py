bb7320f7cfb48ab59d084b5629cc1537ba6c80a8
Brando Erazo <brando.erazo.q@gmail.com>
2026-05-29 07:51:59 -0500

    Create 00_hello.py

---
+++ 01_fundamentos_python/00_hello.py
@@ -0,0 +1,29 @@
+# Hola mundo
+# Nuestro hola mundo en Python
+print("Hola Python")
+
+
+
+
+"""
+Este es un 
+comentario
+en varias líneas
+"""
+
+'''
+Tambien
+se puede escribir comentario
+con esas comillas pero son 3 no ma
+'''
+
+
+
+# Consultar el tipo de dato :
+
+print(type("Hola Python"))   # Tipo :   'str'
+print(type(5))               # Tipo :   'int'
+print(type(1.5))             # Tipo :   'float'
+print(type(1-2j))            # Tipo :   'complex'
+print(type(True))            # Tipo :   'bool'
+print(type(print("Mi cadena de texto")))    # Tipo :   'NoneType'
